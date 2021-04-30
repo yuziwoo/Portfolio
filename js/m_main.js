9e90ad70ff9d768097287d0f83c1c18a744d9353
yuziwoo <yuziwoo@naver.com>
2021-04-30 20:22:51 +0900

    오프닝 완성, 오프닝 다시보기 comment 완성

--- js/m_main.js
+++ js/m_main.js
@@ -2,4 +2,5 @@ window.addEventListener("load", (e) => {
   if (document.body.classList.contains("pc")) {
     window.location.href = "main.html";
   }
+
 })
--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@ window.addEventListener("load", (e) => {
   if (!localStorage.getItem("portfolio1012")[2]) {
     window.location.href = "m_main.html";
   }
+
+  const comment = document.getElementsByClassName("comment")[0];
+
+  document.addEventListener("click", () => {
+    if (comment.style.opacity != "0") {
+       comment.style.opacity = "0";
+       setTimeout(() => {
+         comment.style.display = "none";
+       }, 1000);
+    }
+  });
+  document.addEventListener("wheel", () => {
+    if (comment.style.opacity != "0") {
+       comment.style.opacity = "0";
+       setTimeout(() => {
+         comment.style.display = "none";
+       }, 1000);
+    }
+  });
+
 })
